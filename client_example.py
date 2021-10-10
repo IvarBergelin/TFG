d61e951107c3721bb918d3770c11e5e1a2a2e980
Ivar <ibergeli@uoc.edu>
2021-10-10 21:23:40 +0200

    First commit Message

---
+++ client_example.py
@@ -0,0 +1,95 @@
+import argparse
+import base64
+import json
+
+import cv2
+import numpy as np
+import requests
+
+
+def process_response(response):
+    results = json.loads(response.text)
+    results = results.get('data', response.text)
+    return response.status_code, results
+
+
+def encode_img(img, encoding):
+    _, buf = cv2.imencode(encoding, img)
+    return buf
+
+
+def post_img(url, port, img: np.array, 
+            encoding: str = 'png',
+            save_to: str = '/tmp/image.png'):
+
+    buf = encode_img(img, '.' + encoding)
+    img64 = base64.b64encode(buf)
+
+    req_url = f'{url}:{port}/show'
+    try:
+        r = requests.post(req_url, data={
+            'img': img64,
+            'save_to': save_to,
+        })
+    except ConnectionResetError:
+        return False, None
+
+    return process_response(r)
+
+
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument("image", type=str, help="Path to the image to send to the server.")
+    args.add_argument("--url", type=str, default='http://127.0.0.1', help="Server's url.")
+    args.add_argument("--port", type=int, default=6000, help="Port to listen to incoming requests.")
+    args.add_argument("-s", "--save-to", type=str, default='/tmp/image.png', help="Path to save image on server's side.")
+
+    config = args.parse_args()
+
+    img = cv2.imread(config.image)
+    status_code, bounding_boxes = post_img(url=config.url, port=config.port, img=img, save_to=config.save_to)
+    
+    
+    
+    if status_code == 200:
+        print('Request successful.')
+        # Deserialization
+        print("Decode JSON serialized NumPy array")
+        decodedArrays = json.loads(bounding_boxes)
+        print(f'Bounding boxes: {decodedArrays}')
+        
+        print('type decodedArrays:')
+        print (type(decodedArrays))
+       
+        print('\n')
+        """
+        arrayList = list(decodedArrays.items())
+        
+        print('type arrayList:')
+        print (type(arrayList))
+        print('arrayList:')
+        print (arrayList)
+        print('\n')
+        
+       
+        arrayTemp = np.array(arrayList)
+        arrayTemp = np.asarray(decodedArrays)
+        
+        
+        print('type arrayTemp:')
+        print (type(arrayTemp))
+        print('\n')
+        """
+        
+        for (x,y,w,h) in decodedArrays:
+            print (x,y)
+            
+            cv2.rectangle((img,(x,y),(x+w,y+h)),(255,0,0),2)
+            #roi_gray = gray[y:y+h, x:x+w]
+            #roi_color = img[y:y+h, x:x+w]
+    else:
+        print(f'Request returned error {status_code}')
+
+
+if __name__ == "__main__":
+    main()
---
+++ server_example.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import base64
+import json
+from json import JSONEncoder
+
+
+
+from flask import Flask, Response
+from flask_restful import Resource, Api, reqparse
+import cv2
+import numpy as np
+import pandas as pd
+
+haar_file = (cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(haar_file)
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+
+class Show(Resource):
+    def get(self):
+        data = pd.DataFrame([], columns=['test'])
+        data = data.to_dict()
+        return {'data': data}, 200
+
+
+    def post(self):
+        print('received post request')
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('img', required=True)
+        parser.add_argument('save_to', required=True)
+
+        args = parser.parse_args()
+
+        img = base64.b64decode(args.img)
+        nparr = np.frombuffer(img, np.uint8)
+        img = cv2.imdecode(nparr, flags=1)
+        
+        
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray)
+
+
+        cv2.imwrite(args.save_to, img)
+        
+        # Serialization
+        
+        tempData = np.array(faces)
+        
+        results = {"array": tempData}
+        
+        
+        return Response(
+        
+        response = json.dumps(results, cls=NumpyArrayEncoder),
+        
+   #         response=json.dumps({ 
+   #             "data": results,
+   #         }),
+   
+   
+            status=200,
+            mimetype='application/json'
+        )
+
+
+def start_server(
+    port: int = 6000,
+):
+    app = Flask(__name__)
+    api = Api(app)
+
+    api.add_resource(Show, '/show')
+    app.run(port=port)
+    
+
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument("--port", type=int, default=6000, help="Port to listen to incoming requests.")
+    
+    config = args.parse_args()
+    start_server(config.port)
+    
+
+if __name__ == '__main__':
+    main()
